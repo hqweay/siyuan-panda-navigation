a4918b672fc3b71aee1cca0843afd27c85cf6024
hqweay <hqweay@gmail.com>
2026-07-08 20:55:43 +0800

    feat: update url command to support custom document IDs via openByUrl utility

--- src/builtins/commands/url.ts
+++ src/builtins/commands/url.ts
@@ -1,11 +1,12 @@
 import { BuiltinCommand } from "../types";
+import { openByUrl } from "../../myscripts/syUtils";
 
 export const urlCommand: BuiltinCommand = {
     id: "url",
-    title: "URL 链接",
+    title: "自定义链接/文档ID",
     requiresParam: true,
-    paramPlaceholder: "https:// 或 siyuan://...",
+    paramPlaceholder: "https:// 或 siyuan:// 或 文档ID...",
     execute: (plugin, param) => {
-        if (param) window.open(param);
+        if (param) openByUrl(param);
     }
 };
